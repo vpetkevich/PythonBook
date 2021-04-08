455db0f1ca3836578d88e55bf947755671def8ec
vpetkevich <vpetkevich1994@gmail.com>
2021-04-08 11:59:07 +0300

    completed chapter 12

---
+++ work_with_data/exercice_3.py
@@ -0,0 +1,4 @@
+mystery = '\U0001f4a9'
+pop_bytes = mystery.encode('utf-8')
+pop_string = pop_bytes.decode('utf-8')
+print(pop_string)
---
+++ work_with_data/exercice_5.py
@@ -0,0 +1,4 @@
+import re
+from exercice_4 import mammoth
+
+print(re.findall(r'\bc\w+', mammoth))
